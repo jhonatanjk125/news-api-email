87569ed5e04d90f31001e4bcc61e5be1d9390cf3
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-09-12 00:58:55 -0500

    Send an email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from send_email import send_email
 
 api_key = "65f5c447242040e8b9a5a5ac77678a9d"
 url = "https://newsapi.org/v2/top-headlines?country=us" \
@@ -9,6 +11,14 @@ request = requests.get(url)
 # Get a dictionary with the data
 content = request.json()
 # Print title and content of each article
+raw_message = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    raw_message = f"{raw_message}\n\n{article['title']}\n{article['description']}"
+
+message = f"""\
+Subject: News email
+
+{raw_message}
+"""
+send_email(message.encode("utf-8"))
+
---
+++ send_email.py
@@ -0,0 +1,14 @@
+import smtplib, ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "jhonatanjk125@gmail.com"
+    password = os.getenv("PORTFOLIO-PASSWORD")
+    context = ssl.create_default_context()
+    receiver = "jhonatanjk125@gmail.com"
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
